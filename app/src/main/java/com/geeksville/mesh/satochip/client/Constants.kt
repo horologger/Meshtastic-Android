439840784cf03eeff4b11d00b2e4300517556734
horologger <horologger@protonmail.com>
2025-05-16 14:22:55 -0400

    building/working with satochip libs

---
+++ app/src/main/java/com/geeksville/mesh/satochip/client/Constants.kt
@@ -0,0 +1,44 @@
+package com.geeksville.mesh.satochip.client
+
+object Constants {
+    // APDU constants
+    const val CLA_ISO7816 = 0x00.toByte()
+    const val INS_SELECT = 0xA4.toByte()
+    const val P1_SELECT_BY_NAME = 0x04.toByte()
+    const val P2_SELECT_FIRST_OR_ONLY = 0x00.toByte()
+    
+    // Satochip specific constants
+    const val CLA_SATOCHIP = 0x80.toByte()
+    const val INS_GET_STATUS = 0xF2.toByte()
+    const val INS_VERIFY_PIN = 0x20.toByte()
+    const val INS_CHANGE_PIN = 0x21.toByte()
+    const val INS_UNBLOCK_PIN = 0x22.toByte()
+    const val INS_LOAD_KEY = 0xD0.toByte()
+    const val INS_DERIVE_KEY = 0xD1.toByte()
+    const val INS_GEN_KEY = 0xD2.toByte()
+    const val INS_SIGN = 0xC0.toByte()
+    const val INS_GET_PUBKEY = 0xC1.toByte()
+    
+    // Status words
+    const val SW_SUCCESS = 0x9000
+    const val SW_WRONG_PIN = 0x63C0
+    const val SW_PIN_BLOCKED = 0x63C1
+    const val SW_SECURITY_STATUS_NOT_SATISFIED = 0x6982
+    const val SW_CONDITIONS_NOT_SATISFIED = 0x6985
+    const val SW_WRONG_DATA = 0x6A80
+    const val SW_WRONG_LENGTH = 0x6700
+    const val SW_INS_NOT_SUPPORTED = 0x6D00
+    const val SW_CLA_NOT_SUPPORTED = 0x6E00
+    
+    // Key types
+    const val KEY_TYPE_MASTER = 0x00.toByte()
+    const val KEY_TYPE_AUTHENTICATION = 0x01.toByte()
+    const val KEY_TYPE_ENCRYPTION = 0x02.toByte()
+    const val KEY_TYPE_SIGNATURE = 0x03.toByte()
+    
+    // PIN constants
+    const val PIN_DEFAULT = "123456"
+    const val PIN_MIN_LENGTH = 4
+    const val PIN_MAX_LENGTH = 32
+    const val PIN_MAX_TRIES = 3
+} 
